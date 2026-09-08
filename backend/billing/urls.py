ca20ca9d2351b5728f4947377a1b7e0d59e8d32f
Gerry-01 <odhiambogerry010@gmail.com>
2026-09-08 09:23:17 +0300

    added the url for the cash payment endpoint

--- backend/billing/urls.py
+++ backend/billing/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import bills, patients
+from .views import bills, patients,cash_payment
 
 urlpatterns = [
     path('bills/', bills, name='bills'),
     path('patients/', patients, name='patients'),
+    path('bills/<int:bill_id>/cash_payment/', cash_payment, name='cash_payment'),
 ]
