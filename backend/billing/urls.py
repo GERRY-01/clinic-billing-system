eb724080807a96b7b34c2f4f2fef2e487246201d
Gerry-01 <odhiambogerry010@gmail.com>
2026-09-08 09:37:43 +0300

    added the url for the M-pesa webhook endpoint

--- backend/billing/urls.py
+++ backend/billing/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import bills, patients,cash_payment
+from .views import bills, patients,cash_payment,mpesa_webhook
 
 urlpatterns = [
     path('bills/', bills, name='bills'),
     path('patients/', patients, name='patients'),
     path('bills/<int:bill_id>/cash_payment/', cash_payment, name='cash_payment'),
+    path('payments/mpesa_webhook/', mpesa_webhook, name='mpesa_webhook'),
 ]
